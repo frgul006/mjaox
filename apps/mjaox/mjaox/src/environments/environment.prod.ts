f3a5e344d37ae55acefa0dd80812532c748628ee
Fredrik Gullberg <frgul006@hotmail.com>
2018-11-05 23:21:59 +0100

    Firebase

--- apps/mjaox/mjaox/src/environments/environment.prod.ts
+++ apps/mjaox/mjaox/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB3R4blTRnF0O12lDpjlcHXQaIaFyuU47o',
+    authDomain: 'mjaox-9df13.firebaseapp.com',
+    databaseURL: 'https://mjaox-9df13.firebaseio.com',
+    projectId: 'mjaox-9df13',
+    storageBucket: 'mjaox-9df13.appspot.com',
+    messagingSenderId: '821543715748'
+  }
 };
